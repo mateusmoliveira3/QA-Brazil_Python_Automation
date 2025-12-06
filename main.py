905b80abd5813241b5b965687c9999446a28f7a2
Mateus Marques <mateusmoliveira3@gmail.com>
2025-12-06 20:56:27 -0300

    commit depois de inserir code no helpers

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
 
 class TestUrbanRoutes:
+    def test_set_route(self):
+        # Adicionar em S8
+        print("função criada para definir a rota")
+        pass
+
+    def test_select_plan(self):
+        # Adicionar em S8
+        print("função criada para definir o plano")
+        pass
+
+    def test_fill_phone_number(self):
+        # Adicionar em S8
+        print("função criada para definir o numero de telefone")
+        pass
+
+    def test_fill_card(self):
+        # Adicionar em S8
+        print("função criada para definir preencher cartao")
+        pass
+
+    def test_comment_for_driver(self):
+        # Adicionar em S8
+        print("função criada para definir motorista")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Adicionar em S8
+        print("função criada para definir blanket and handkerchiefs")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Adicionar em S8
+        print("função criada para definir o sorvete")
+        pass
+
+    def test_car_search_model_appears(self):
+        # Adicionar em S8
+        print("função criada para definir modelo")
+        pass
