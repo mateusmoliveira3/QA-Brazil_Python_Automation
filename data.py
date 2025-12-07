db5874dc716d71ec0474f185a479fa03b9ba040e
Mateus Marques <mateusmoliveira3@gmail.com>
2025-12-06 21:30:00 -0300

    commit depois de inserir code no helpers

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 # Arquivo com constantes
-URBAN_ROUTES_URL = ''
+URBAN_ROUTES_URL = 'https://cnt-dc5a6285-4b5b-4fcc-9616-ee3f6407e663.containerhub.tripleten-services.com/?lng=pt'
 ADDRESS_FROM = 'East 2nd Street, 601'
 ADDRESS_TO = '1300 1st St'
 PHONE_NUMBER = '+1 123 123 12 12'
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import data
+import helpers
 
 class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("conectado ao Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+
     def test_set_route(self):
         # Adicionar em S8
         print("função criada para definir a rota")
@@ -31,8 +40,10 @@ class TestUrbanRoutes:
         pass
 
     def test_order_2_ice_creams(self):
-        # Adicionar em S8
-        print("função criada para definir o sorvete")
+        numbers_of_ice_creams = 2
+        for count in range(numbers_of_ice_creams):
+            # Adicionar em S8
+            print("função criada para definir o sorvete")
         pass
 
     def test_car_search_model_appears(self):
